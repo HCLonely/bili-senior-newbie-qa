e1c44ce838b404f000b05ef4d6ca7d82d082c1d6
HCLonely <h1606051253@gmail.com>
2025-02-12 17:11:21 +0800

    添加GPT版本

--- README.md
+++ README.md
@@ -3,9 +3,25 @@
 ## 安装
 
 1. 安装[Tampermonkey](https://www.tampermonkey.net/index.php)(建议)或其他脚本管理器；
-2. [点此安装](https://github.com/HCLonely/bili-senior-newbie-qa/blob/main/bili-senior-newbie-qa.user.js?raw=true)搜题脚本。
+2. 安装搜题脚本。
+    - [点此安装](https://github.com/HCLonely/bili-senior-newbie-qa/blob/main/bili-senior-newbie-qa-GPT.user.js?raw=true)GPT版;
+    - [点此安装](https://github.com/HCLonely/bili-senior-newbie-qa/blob/main/bili-senior-newbie-qa.user.js?raw=true)旧版。
 
-## 使用
+## 使用-GPT版
+
+1. ~~手机哔哩哔哩客户端进入答题；~~
+2. 浏览器登录哔哩哔哩(已登录可跳过此步骤)；
+3. 浏览器打开调试工具(`F12`), 将浏览器切换为移动设备(`Ctrl+Shift+M`)；
+4. 在地址栏输入[https://www.bilibili.com/h5/senior-newbie/](https://www.bilibili.com/h5/senior-newbie/)并回车，进入答题，等待网页加载完成；
+5. 点击浏览器上的Tampermonkey扩展图标（一般在右上角）；
+6. （首次使用需要）点击`哔哩哔哩硬核会员搜题GPT`下的`设置 ChatAnywhere API 密钥`或`设置 DeepSeek API 密钥`，输入你的密钥；
+    - [获取ChatAnywhere API 密钥](https://github.com/chatanywhere/GPT_API_free?tab=readme-ov-file#%E5%85%8D%E8%B4%B9%E4%BD%BF%E7%94%A8)(免费，准确率较`DeepSeek`低)
+    - [获取DeepSeek API 密钥](https://platform.deepseek.com/)(付费，准确率较高)
+7. 点击`哔哩哔哩硬核会员搜题GPT`下的`启动`。
+
+> PS: 启动后不要点击页面的任何位置，可以在右侧的控制台查看运行进度！
+
+## 使用-旧版
 
 1. ~~手机哔哩哔哩客户端进入答题；~~
 2. 浏览器登录哔哩哔哩(已登录可跳过此步骤)；
---
+++ bili-senior-newbie-qa-GPT.user.js
@@ -0,0 +1,182 @@
+// ==UserScript==
+// @name         哔哩哔哩硬核会员搜题GPT
+// @namespace    bili-senior-newbie-qa-GPT
+// @version      1.1
+// @description  哔哩哔哩硬核会员搜题GPT
+// @author       HCLonely
+// @include      *://www.bilibili.com/h5/senior-newbie/qa*
+// @run-at       document-end
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_xmlhttpRequest
+// @grant        GM_notification
+// @homepage     https://github.com/HCLonely/bili-senior-newbie-qa
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.slim.min.js
+// @license      Apache-2.0
+// @connect      api.deepseek.com
+// @connect      api.chatanywhere.tech
+// ==/UserScript==
+
+"use strict";
+/* eslint-disable no-underscore-dangle, @typescript-eslint/no-empty-function */
+(async () => {
+  window.onblur = () => { };
+  window.onfocus = () => { };
+  document.onfocusin = () => { };
+  document.onfocusout = () => { };
+  document._addEventListener = document.addEventListener;
+  document.addEventListener = (...argv) => {
+    if (['visibilitychange', 'mozvisibilitychange', 'webkitvisibilitychange', 'msvisibilitychange'].includes(argv[0])) {
+      return;
+    }
+    document._addEventListener(...argv);
+  };
+  document._removeEventListener = document.removeEventListener;
+  document.removeEventListener = (...argv) => {
+    if (['visibilitychange', 'mozvisibilitychange', 'webkitvisibilitychange', 'msvisibilitychange'].includes(argv[0])) {
+      return;
+    }
+    document._removeEventListener(...argv);
+  };
+  window.onload = () => {
+    window.onblur = () => { };
+    window.onfocus = () => { };
+    document.onfocusin = () => { };
+    document.onfocusout = () => { };
+  };
+
+  let { API_KEY, GPT_TYPE } = GM_getValue('API_KEY');
+  let prevQuestion = '';
+  GM_registerMenuCommand('设置 ChatAnywhere API 密钥', () => {
+    const key = prompt('请输入 ChatAnywhere API 密钥：', API_KEY);
+    if (key !== null) {
+      API_KEY = key;
+      GPT_TYPE = 'ChatAnywhere';
+      GM_setValue('API_KEY', { API_KEY, GPT_TYPE });
+    }
+  });
+  GM_registerMenuCommand('设置 DeepSeek API 密钥', () => {
+    const key = prompt('请输入 DeepSeek API 密钥：', API_KEY);
+    if (key !== null) {
+      API_KEY = key;
+      GPT_TYPE = 'DeepSeek';
+      GM_setValue('API_KEY', { API_KEY, GPT_TYPE });
+    }
+  });
+  GM_registerMenuCommand('启动', () => {
+    if (!API_KEY) {
+      alert('请先设置 API 密钥！');
+      return;
+    }
+    console.clear();
+    alert('启动后不要点击页面！');
+    document.dispatchEvent(new Event('click'));
+  });
+  document.addEventListener('click', start);
+
+  async function start(retry = 0) {
+    const delayTime = Math.floor(Math.random() * (30000 - 10000 + 1)) + 10000;
+    console.log(`等待 ${delayTime / 1000} 秒后开始搜索下一题`);
+    await sleep(delayTime);
+    const question = $('div.senior-question').find('.senior-question__qs,.senior-question__answer').toArray().map((v) => $(v).text().trim()).join('\n');
+    if (question === prevQuestion) {
+      if (retry > 3) {
+        return GM_notification({
+          title: '哔哩哔哩硬核会员搜题GPT',
+          text: '重试次数已超过3次，请刷新页面重试或稍后再试。',
+          timeout: 10000
+        });
+      }
+      start(++retry);
+    }
+    console.log(question);
+    await askGPT(question);
+  }
+
+  function askGPT(question, retry = 0) {
+    return callGPTAPI(`请仅给出答案\n${question}`).then((answer) => {
+      let option = answer.match(/\w/)?.[0]?.toUpperCase();
+      if (!['A', 'B', 'C', 'D'].includes(option)) {
+        const letters = 'ABCD';
+        option = letters[Math.floor(Math.random() * letters.length)];
+      }
+      console.log(`选择答案：${option}`);
+      buttonClick($('span.senior-question__answer--icon:contains(' + option + ')').parent()[0]);
+    }).catch(async (error) => {
+      if (retry > 3) {
+        return GM_notification({
+          title: '哔哩哔哩硬核会员搜题GPT',
+          text: '重试次数已超过3次，请刷新页面重试或稍后再试。',
+          timeout: 10000
+        });
+      }
+      await sleep(5000);
+      askGPT(++retry);
+    });
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function buttonClick(button) {
+    const rect = button.getBoundingClientRect();
+    const randomX = rect.left + Math.random() * rect.width;
+    const randomY = rect.top + Math.random() * rect.height;
+    const clickEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      clientX: randomX,
+      clientY: randomY,
+    });
+    button.dispatchEvent(clickEvent);
+  }
+
+  const API_URL = {
+    'ChatAnywhere': 'https://api.chatanywhere.tech/v1/chat/completions',
+    'DeepSeek': 'https://api.deepseek.com/v1/chat/completions'
+  };
+  const GPT_MODEL = {
+    'ChatAnywhere': 'gpt-4o-mini',
+    'DeepSeek': 'deepseek-chat'
+  };
+  function callGPTAPI(question) {
+    return new Promise((resolve, reject) => {
+      const apiUrl = API_URL[GPT_TYPE];
+      const requestData = {
+        "messages": [
+          {
+            "content": question,
+            "role": "system"
+          }
+        ],
+        temperature: 0.7,
+        "model": GPT_MODEL[GPT_TYPE],
+        "stream": false,
+        "response_format": {
+          "type": "text"
+        }
+      };
+      GM_xmlhttpRequest({
+        method: 'POST',
+        url: apiUrl,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${API_KEY}`,
+        },
+        data: JSON.stringify(requestData),
+        onload: function (response) {
+          // console.log('API 响应:', response.responseText);
+          const data = JSON.parse(response.responseText);
+          console.log('API 返回结果: ' + data.choices[0].message.content);
+          resolve(data.choices[0].message.content);
+        },
+        onerror: function (error) {
+          console.error('API 请求失败:', error);
+          reject(error);
+        },
+      });
+    });
+  }
+})();
